a135f1bb444c084151e41c03fb970842b0dc5454
Sourav <souravunofficial000@gmail.com>
2025-05-11 17:29:58 +0530

    made server more secure(class based),

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-// import { connectToMongoAtlas } from "./infrastructure/DB/mongo.config";
-import express from "express";
-import { Server_Config } from "./configs/server/Server.Config";
-import { serverMiddlewareConfig } from "./infrastructure/presentation/middleware/express";
+import { Server } from "infrastructure/presentation/server";
 
-const app = express();
-const { PORT_NO } = Server_Config();
+const ChitChat = new Server();
 
-const Server = async () => {
-  try {
-    serverMiddlewareConfig(app);
-
-    app.listen(PORT_NO, () => {
-      console.info(`Server running on PORT: ${PORT_NO}`);
-    });
-
-    //     await connectToMongoAtlas().then();
-
-    // app.use("/api/v1/auth/user");
-    // app.use("/api/v1/user");
-  } catch (error: any) {
-    console.error("Error in starting server:", error);
-  }
-};
-
-Server();
+ChitChat.start();
